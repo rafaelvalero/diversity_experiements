1a44fe7556d77405834de45adcae2f8ed2104fec
Rafael Valero-Fernandez <rafaelvalerofernandez@gmail.com>
2024-06-25 17:23:18 +0100

    creating example

--- exp_ecopy.py
+++ exp_ecopy.py
@@ -3,9 +3,23 @@ Measurements of diversity:
 Implementation in python of diversity metrics --> Ecopy: https://ecopy.readthedocs.io/en/latest/diversity.html
      * Nate Lemoine, & Matt Gregory. (2015). ecopy: First release (v0.8). Zenodo. https://doi.org/10.5281/zenodo.29232
 """
+import pandas as pd
 
-import main
-import utils
+from main import *
+from utils import *
 
-varespec = utils.load_data('varespec')
-shannonH = main.diversity(varespec, 'shannon')
+data = load_data('varespec')
+shannonH = diversity(data, 'shannon')
+
+list_of_diversity_metrics = ['shannon', 'gini-simpson', 'simpson', 'dominance', 'spRich', 'even']
+
+report_list = []
+for diversity_metric in list_of_diversity_metrics:
+    print(diversity_metric)
+    aux = diversity(data.T,
+                    method=diversity_metric)
+    aux = pd.DataFrame(aux, index=data.columns, columns = [diversity_metric])
+    report_list.append(aux)
+
+output = pd.concat(report_list, axis = 1)
+print(output)
