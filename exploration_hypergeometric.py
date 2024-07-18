5e0a2decddb4e37c3981a1a7825971acc4ba594b
Rafael Valero-Fernandez <rafaelvalerofernandez@gmail.com>
2024-07-18 13:58:16 +0100

    adding hypergeometric analysis example

---
+++ exploration_hypergeometric.py
@@ -0,0 +1,6 @@
+"""
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.multivariate_hypergeom.html
+"""
+
+from scipy.stats import multivariate_hypergeom
+multivariate_hypergeom.pmf(x=[8, 4], m=[10, 20], n=12)
