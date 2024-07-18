ad447846d85dc4885e68a26ba96f90b064b0e026
Rafael Valero-Fernandez <rafaelvalerofernandez@gmail.com>
2024-07-18 17:21:45 +0100

    adding a few examples to compare

--- exploration_hypergeometric.py
+++ exploration_hypergeometric.py
@@ -1,6 +1,8 @@
 """
 We could calculate the probability of the sample.
 https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.multivariate_hypergeom.html
+https://en.wikipedia.org/wiki/Hypergeometric_distribution
+
 """
 
 from scipy.stats import multivariate_hypergeom
@@ -23,7 +25,7 @@ print(multivariate_hypergeom.pmf(x=sample, m=population, n=np.sum(sample)))
 
 """Let explore vs rarity from greylock
 """
-"""
+
 samples_list = [[2, 0],
                 [1, 1],
                 [0, 2],
@@ -32,6 +34,7 @@ samples_list = [[2, 0],
 samples_list = [[2, 0],
                 [1, 1],
                 [0, 2]]
+"""
 
 list_ = []
 for sample in samples_list:
@@ -59,9 +62,16 @@ for sample in samples_list:
                                                                measure="rho")[0]
         dict_['normalized_rho'] = metacommunity_1a.subcommunity_diversity(viewpoint=0,
                                                                           measure="normalized_rho")[0]
+        dict_['gamma'] = metacommunity_1a.subcommunity_diversity(viewpoint=0,
+                                                                 measure="gamma")[0]
+        dict_['normalized_alpha'] = metacommunity_1a.subcommunity_diversity(viewpoint=0,
+                                                                            measure="normalized_alpha")[0]
+        dict_['alpha'] = metacommunity_1a.subcommunity_diversity(viewpoint=0,
+                                                                 measure="alpha")[0]
     except Exception as e:
         print(e)
     list_.append(dict_)
 # comparing
-aux = pd.DataFrame(list_)
-print(aux)
+comparison = pd.DataFrame(list_).round(2)
+#comparison.round(2)
+print(comparison)
